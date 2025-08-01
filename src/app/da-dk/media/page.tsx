43453e681e574af36f8c4c402da3b16502f4c944
Rachel Evans <rachel@rachelevans.org>
2025-08-01 16:14:18 +0200

    feat: media focus

---
+++ src/app/da-dk/media/page.tsx
@@ -0,0 +1,27 @@
+import CV from "../page";
+
+const page = () => (
+  <CV
+    pitchTitle="Software Ingeniør og Teknisk Leder, tidligere hos BBC"
+    pitchText={
+      <>
+        <p>
+          Jeg er en dedikeret software ingeniør, der specialiserer mig i
+          back-end udvikling med dygtighed også til full-stack. I 11 år
+          arbejdede jeg hos BBC i London, da vi stod til ansvar for al BBCs
+          online audio og video (herunder BBC iPlayer). Cirka 2013 lavede vi
+          hele online-media-systemet om til at være cloud-native: indtagning,
+          transcode, metadata opdatering.
+        </p>
+        <p>
+          Jeg elsker at lave kode til høj kvalitet, sikkerhed og
+          vedligeholdelse, og at tage ejerskab hele vejen fra undfangelse til
+          production til en endelig nedlæggelse. Jeg elsker at dele ting, som
+          jeg lærer, med folk omkring mig, også at lære af dem.
+        </p>
+      </>
+    }
+  />
+);
+
+export default page;
