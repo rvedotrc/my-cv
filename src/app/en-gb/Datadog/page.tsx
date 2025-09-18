9d61796f4897fa75001176eb6e87b8b9e42a4ed5
Rachel Evans <rachel@rachelevans.org>
2025-09-18 11:58:05 +0200

    feat: datadog

---
+++ src/app/en-gb/Datadog/page.tsx
@@ -0,0 +1,27 @@
+import CV from "../page";
+
+const page = () => (
+  <CV
+    pitchTitle="Experienced Software Engineer and Technical Leader"
+    pitchText={
+      <>
+        <p>
+          Over the last 15+ years I have a proven track record of bringing
+          expertise, leadership, and mentoring to the teams I work with, first
+          at the BBC in London, and subsequently at Zendesk in Copenhagen. I
+          work with others to design and build solid, secure, performant
+          systems, taking care of the whole lifecycle from conception to
+          decommissioning.
+        </p>
+        <p>
+          During my 5+ years at Zendesk, I used Datadog extensively, in
+          particular as a maintainer of Zendesk's in-house IaC tool for Datadog.
+          I have always had a strong interest in reliability, performance, and
+          security, and I love learning from others, and sharing what I know.
+        </p>
+      </>
+    }
+  />
+);
+
+export default page;
